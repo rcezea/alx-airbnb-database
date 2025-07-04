6876eb667c498feed8fd04dbd376b6e55eef96d7
Richard <rcezea007@gmail.com>
2025-07-04 10:02:19 +0100

    Apply Aggregations and Window Functions

---
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -0,0 +1,24 @@
+-- Write a query to find the total number
+-- of bookings made by each user, using
+-- the COUNT function and GROUP BY clause.
+
+SELECT first_name, COUNT(B.booking_id)
+FROM User
+INNER JOIN Booking B ON User.user_id = B.user_id
+GROUP BY User.user_id
+HAVING COUNT(B.booking_id) >= 3;
+
+-- Use a window function (ROW_NUMBER, RANK)
+-- to rank properties based on the total number
+-- of bookings they have received.
+
+SELECT name, booking_count,
+    RANK() OVER (ORDER BY booking_count) AS booking_rank
+FROM (
+    SELECT
+        P.name,
+        COUNT(B.booking_id) AS booking_count
+    FROM Property P
+    INNER JOIN Booking B on P.property_id = B.property_id
+    GROUP BY P.property_id, P.name
+) as PBbc;
